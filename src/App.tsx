5aed9e06dc0c4c85ab6313bf3c3289066624dbb6
Tyler Kimball <tkimball@dexwell.com>
2023-03-06 10:46:59 -0800

    start routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes, Navigate } from 'react-router-dom'
 
 function App() {
 
   return (
-    <div>
-      hi
-    </div>
+    <Routes>
+        <Route path='/' element={<h1>Hi</h1>} />
+        <Route path='/new' element={<h1>New</h1>} />
+        <Route path='/:id' ></Route>
+        <Route path='*' element={<Navigate to="/" />} />
+    </Routes>
   )
 }
 
